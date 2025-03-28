9fd80221ae91ba5dffd48d04a5032ffd8c0960c7
F3dd3 <97116630@st.deltion.nl>
2025-03-28 12:40:06 +0100

    lantaarn

--- Assets/Scripts/save and load.cs
+++ Assets/Scripts/save and load.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
-public class saveandload : MonoBehaviour
+
+[System.Serializable]
+public class PlayerData
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health;
+    public Vector3 position;
+    public int score;
+}
+
+public class SaveLoadsystem : MonoBehaviour
+{
+    private string saveFilePath;
+
+    private void Awake()
     {
-        
+        // Set the file path (you can adjust this to your preference)
+        saveFilePath = Path.Combine(Application.persistentDataPath, "playerSaveData.json");
     }
 
-    // Update is called once per frame
-    void Update()
+    // Save data to a file
+    public void SavePlayerData(PlayerData playerData)
     {
-        
+        string json = JsonUtility.ToJson(playerData);
+        File.WriteAllText(saveFilePath, json);
+        Debug.Log("Game Saved!");
+    }
+
+    // Load data from a file
+    public PlayerData LoadPlayerData()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            string json = File.ReadAllText(saveFilePath);
+            PlayerData playerData = JsonUtility.FromJson<PlayerData>(json);
+            Debug.Log("Game Loaded!");
+            return playerData;
+        }
+        else
+        {
+            Debug.Log("No save file found.");
+            return null;
+        }
     }
 }
