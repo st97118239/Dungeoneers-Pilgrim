f686b449affb9ea5ceb29005138654dac45ad01d
F3dd3 <97116630@st.deltion.nl>
2025-03-31 10:28:22 +0200

    Json knop save

--- Assets/Scripts/Save and load/Json.cs
+++ Assets/Scripts/Save and load/Json.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO; 
 
-public class Json : MonoBehaviour
+public class SaveSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static string saveFilePath = Application.persistentDataPath + "/savefile.json";
+
+    public static void Save(SaveData data)
     {
-        
+        // Convert the data to a JSON string 
+        string json = JsonUtility.ToJson(data);
+
+        // Save the JSON string to a file 
+        File.WriteAllText(saveFilePath, json);
+
+        Debug.Log("Game Saved at " + saveFilePath);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static SaveData Load()
     {
-        
+        if (File.Exists(saveFilePath))
+        {
+            // Read the JSON string from the file 
+            string json = File.ReadAllText(saveFilePath);
+
+            // Deserialize the JSON into a SaveData object 
+            SaveData loadedData = JsonUtility.FromJson<SaveData>(json);
+
+            Debug.Log("Game Loaded from " + saveFilePath);
+            return loadedData;
+        }
+        else
+        {
+            Debug.LogError("Save file does not exist.");
+            return null; // Or return a default SaveData instance if preferred 
+        }
     }
 }
